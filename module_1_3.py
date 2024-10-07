d720150b8d109ef2c849a1b401604e1ca52e72e8
Alexey11210 <ebkronshtat@gmail.com>
2024-10-08 09:37:45 +1000

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = 'Алексей'
+print(name, type(name))
+age = 34
+print(age, type(age))
+new_age = 15
+print((age) + (new_age))
+new_age = 17
+print((age) + (new_age))
+is_student = True
+print(is_student)
+print((is_student), type(is_student))
