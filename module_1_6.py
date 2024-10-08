3a0c8a800805e0962f278fde81a31db712d32c0e
Alexey11210 <ebkronshtat@gmail.com>
2024-10-08 11:40:10 +1000

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+# Алексей
+number_of_completed_exercises = 12
+number_of_spent_hours = 1.5
+name_of_course = 'Python,'
+print(str('Курс:'), name_of_course, str('всего задач:'), number_of_completed_exercises, str('затрачено часов:'), number_of_spent_hours, str('среднее время выполнения'), number_of_spent_hours/number_of_completed_exercises, str('часа.'))
+n_1 = 12
+n_2 = 1.5
+n_3 = 'Python,'
+print(str('Курс:'), n_3, str('всего задач:'), n_1, str('затрачено часов:'), n_2, str('среднее время выполнения'), n_2/n_1, str('часа.'))
