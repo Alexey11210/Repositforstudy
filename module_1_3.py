353cde23f0335218063d484c7d1674542162599f
Alexey11210 <ebkronshtat@gmail.com>
2024-10-10 10:24:44 +1000

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,11 +1,10 @@
 name = 'Алексей'
-print(name, type(name))
+print(name)
 age = 34
-print(age, type(age))
-new_age = 15
-print((age) + (new_age))
-new_age = 17
-print((age) + (new_age))
+print(age)
+age = (age + 15)
+print(age)
+age = (age + 17)
+print(age)
 is_student = True
 print(is_student)
-print((is_student), type(is_student))
