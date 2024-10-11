fad6f62c2c838793ee4d08cc8548dc9167378a83
Alexey11210 <ebkronshtat@gmail.com>
2024-10-11 11:07:50 +1000

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,7 +1,10 @@
 my_string = input('Какие новости на сегодня? ')
 print('ну надо же,', my_string.upper(), ',да ладно?')
-my_string_1 = input('а еще что интересного? ')
+print(len(my_string))
+my_string_1 = input('А еще что интересного? ')
 print('во дела,', my_string_1.lower(), ',капец новости конечно...')
+print(len(my_string_1))
 print(my_string.replace(' ',''))
 print(my_string[0])
 print(my_string[-1])
+
