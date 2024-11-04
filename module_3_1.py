47167777eadd3990f70a15962851628e3741bbbf
Alexey11210 <ebkronshtat@gmail.com>
2024-11-04 17:24:46 +1000

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,29 +1,21 @@
 calls = 0
-
+def count_calls():
+    global calls
+    calls += 1
 def string_info(string):
-    global info
+    count_calls()
     info = len(string), string.upper(), string.lower()
     print(info)
-(string_info('capybara'))
-(string_info('Armageddon'))
-(string_info('Antapka'))
-
-
 def is_contains(string, list_to_search):
-    global nancy
+    count_calls()
     nancy = True
     if string.lower() in list_to_search:
         print(nancy)
     else:
         nancy = False
+        print(nancy)
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', ('urBAN'.lower())]))
 print(is_contains('cycle', ['recycling', 'cyclic']))
-
-
-def count_calls(calls):
-    global calls
-    if info:
-        calls += 1
-    elif nancy:
-        calls += 1
 print(calls)
