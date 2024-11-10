294343cff9dfef7af41d829327baa319cd24328d
Alexey11210 <ebkronshtat@gmail.com>
2024-11-10 10:40:22 +1000

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,10 +1,10 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for nancy in other_words:
-        if root_word in nancy:
+        if root_word.lower() in nancy.lower or nancy.lower() in root_word.lower():
             same_words.append(nancy)
-        elif nancy.lower() in root_word.lower():
-            same_words.append(nancy)
+        # elif nancy.lower() in root_word.lower():
+        #     same_words.append(nancy)
     return same_words
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
