cc2cd39e8e9bd3fdcbbafae7923da9a6381f78a3
Alexey11210 <ebkronshtat@gmail.com>
2024-11-17 13:18:53 +1000

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fake_M
+
+import true_math as true_M
+
+result1 = fake_M.divide(69, 3)
+result2 = fake_M.divide(3, 0)
+result3 = true_M.divide(49, 7)
+result4 = true_M.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
