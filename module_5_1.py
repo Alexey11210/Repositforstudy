f5fb19a711471fcbf6571425c00c25c07a11f7c7
Alexey11210 <ebkronshtat@gmail.com>
2024-11-25 08:03:31 +1000

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,17 @@
+class House:
+    def  __init__(self, company_name, number_of_floors):
+        self.name = company_name
+        self.number_of_floors = number_of_floors
+        print(f'Вас приветствует {self.name} Количество этажей: {self.number_of_floors}')
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > (self.number_of_floors):
+            print(f'В {self.name} Такого этажа не существует')
+        else:
+            print(f'Вас приветствует {self.name}, Ваш выбранный этаж:, {int(new_floor)} из {self.number_of_floors}')
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h3 = House('ЖК Город 4212', 9)
+h1.go_to(5)
+h2.go_to(10)
+h3.go_to(11)
